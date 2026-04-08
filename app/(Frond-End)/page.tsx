9f327f89aab7ec6cd70e41f5c10ddc232c0939b7
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-08 10:47:54 +0600

    💄design (platform): design platform work section

--- app/(Frond-End)/page.tsx
+++ app/(Frond-End)/page.tsx
@@ -1,6 +1,7 @@
 import AppBackground from "./_components/AppBackground";
 import HeroSection from "./_components/Hero/HeroSection";
 import ConnectPeople from "./_components/connect-people/ConnectPeople";
+import PlatformWork from "./_components/platform/PlatformWork";
 
 
 export default function Home() {
@@ -10,6 +11,9 @@ export default function Home() {
         <HeroSection />
       </AppBackground>
       <ConnectPeople />
+      <div className="bg-[#FAFAFA]">
+        <PlatformWork />
+      </div>
     </div>
   );
 }
