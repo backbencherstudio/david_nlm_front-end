c119fb63dabe002df03e538a40aeb7b727f7c83d
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-08 16:34:01 +0600

    🐛 fix(image) :fix the image size

--- app/(Frond-End)/_components/zero-hassle/ZeroHassle.tsx
+++ app/(Frond-End)/_components/zero-hassle/ZeroHassle.tsx
@@ -7,8 +7,8 @@ import Image from 'next/image'
 const ZeroHassle = () => {
     return (
         <Container className='py-10 sm:py-30 '>
-            <div className='flex flex-col lg:flex-row gap-8 lg:gap-25 items-center bg-bgColorOne rounded-2xl p-4 sm:p-10 border border-borderColor'>
-                <div className='w-full lg:w-1/2 space-y-4 sm:space-y-9'>
+            <div className='flex flex-col lg:flex-row justify-between gap-8 lg:gap-15 items-center bg-bgColorOne rounded-2xl  border border-borderColor'>
+                <div className='w-full lg:w-1/2 space-y-4 sm:space-y-9 p-4 sm:p-10'>
                     <div className='space-y-4'>
                         <h2 className='text-2xl sm:text-[2.5rem] font-semibold text-blackColor leading-[130%] capitalize text-center md:text-left'>One Platform, Unlimited Services, and Zero Hassle</h2>
                         <p className='text-sm sm:text-base font-normal text-descriptionColor leading-[160%] max-w-full sm:max-w-[700px] text-center md:text-left'>One powerful platform that brings together endless services, designed to simplify your experience & eliminate every unnecessary step</p>
@@ -18,8 +18,8 @@ const ZeroHassle = () => {
                     </div>
                 </div>
 
-                <div className='w-full lg:w-1/2'>
-                    <Image src="/zeroHassle.png" alt="zeroHassle" width={483} height={500} className='object-contain' />
+                <div className='w-full lg:w-1/2 pt-12.5 pr-10'>
+                    <Image src="/zHassle.png" alt="zeroHassle" width={483} height={500} className='w-full h-full object-contain' />
                 </div>
             </div>
         </Container>
