736fafd5f9b45868ae78f32dcb289e9c599419dd
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-11 08:43:54 +0600

    design: add confirm password design

---
+++ components/Dashboard/confirmation-pass/ConfirmationPassForm.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import CircleTickIcon from "@/icons/CircleTickIcon";
+import GenericButton from "@/app/(auth)/_components/GenericButton";
+
+const ConfirmationPassForm = () => {
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prev) => !prev);
+    };
+    return (
+        <div className="space-y-8">
+           
+            <GenericButton variant="primary" size="md" rounded="2xl" fullWidth onClick={() => console.log("Logged in")}>
+                Proceed to login
+            </GenericButton>
+        </div>
+    );
+};
+
+export default ConfirmationPassForm;
