8e6be6aa737eccd20dbc268254b975159dd9e81f
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-15 11:53:49 +0600

    design: add dynamic data in bookings page

--- components/Dashboard/bookings/BookingsPageContent.tsx
+++ components/Dashboard/bookings/BookingsPageContent.tsx
@@ -1,6 +1,9 @@
 "use client"
+import DynamicTable from '@/components/reusable/DynamicTable'
 import GenericButton from '../auth/GenericButton'
 import DataFilterBar from '../common/DataFilterBar'
+import { bookingsData } from '@/data/booking'
+import { BOOKING_COLUMNS } from './tableConfig'
 
 const BookingsPageContent = () => {
     return (
@@ -13,7 +16,19 @@ const BookingsPageContent = () => {
                 <GenericButton variant='plain' size='xl' rounded='2xl'>Home based booking</GenericButton>
                 <GenericButton variant='plain' size='xl' rounded='2xl'>Location based booking</GenericButton>
             </div>
-
+            <div className='mt-6'>
+                <DynamicTable
+                    border={false}
+                    data={bookingsData}
+                    columns={BOOKING_COLUMNS}
+                    currentPage={1}
+                    itemsPerPage={10}
+                    onPageChange={() => { }}
+                    totalpage={1}
+                    totalItems={10}
+                    setItemsPerPage={() => { }}
+                    onView={() => { }} />
+            </div>
         </div>
     )
 }
